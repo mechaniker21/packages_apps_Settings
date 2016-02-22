318252dde44dd3fccea0290adb787ca4d68678ee
Pafcholini <nadyaivanova14@gmail.com>
2016-02-22 13:24:34 +0100

    Add Kernel Adiutor to Settings Dashboard Credits to  fusionjack
    Signed-off-by: Pafcholini <nadyaivanova14@gmail.com>

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -1342,6 +1342,15 @@ public class SettingsActivity extends Activity
                     if (!supported) {
                         removeTile = true;
                     }
+		 }else if (id == R.id.kernel_adiutor) {
+                    boolean supported = false;
+                    try {
+                        supported = (getPackageManager().getPackageInfo("com.grarak.kerneladiutor", 0).versionCode > 0);
+                    } catch (PackageManager.NameNotFoundException e) {
+                    }
+                    if (!supported) {
+                        removeTile = true;
+                   }
                 } else if (id == R.id.viper_settings) {
                     // Embedding into Settings only if app exists (user could manually remove it)
                     boolean supported = false;
