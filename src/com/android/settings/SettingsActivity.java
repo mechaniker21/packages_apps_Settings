28da6d8cd85c887f6a7309d9689e73ae5e0af00a
Pafcholini <nadyaivanova14@gmail.com>
2016-03-23 00:03:00 +0100

    Add kcal to display
    Signed-off-by: Pafcholini <nadyaivanova14@gmail.com>

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -1361,6 +1361,15 @@ public class SettingsActivity extends Activity
                     if (!supported) {
                         removeTile = true;
                     }
+		 }else if (id == R.id.kcal_display) {
+                    boolean supported = false;
+                    try {
+                        supported = (getPackageManager().getPackageInfo("re.codefi.savoca.kcal", 0).versionCode > 0);
+                    } catch (PackageManager.NameNotFoundException e) {
+                    }
+                    if (!supported) {
+                        removeTile = true;
+                   }
                 }
 
                 if (UserHandle.MU_ENABLED && UserHandle.myUserId() != 0
