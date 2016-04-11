34697e8e0f33a8a97ed929bad2913fae7ae67ce1
bigrushdog <randall.rushing@gmail.com>
2016-04-11 16:57:57 +0200

    [SQUASHED] Settings : Add DU Fling,Pulse,SmartBar
        Action Picker Dialog :
    
        http://gerrit.dirtyunicorns.com/23916
    
        Author:Randall Rushing <randall.rushing@gmail.com>
    
        Expose Navbar for External Fragment Launching
    
        http://gerrit.dirtyunicorns.com/23914
    
        Author:Randall Rushing <randall.rushing@gmail.com>
    
        Settings: Support Bringup
    
        http://gerrit.dirtyunicorns.com/24717
    
        Author:Randall Rushing <randall.rushing@gmail.com>
    
        Revert old options :-
        SLim Dim
        CM navbar
        Slim Navbar Dimensions
    
        Emotionfy:
        - BIG THANKS TO RR team
        - Bring to Emotion
    
    Signed-off-by: placiano <placiano80@gmail.com>

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -72,6 +72,9 @@ import com.android.settings.accessibility.CaptionPropertiesFragment;
 import com.android.settings.accounts.AccountSettings;
 import com.android.settings.accounts.AccountSyncSettings;
 import com.android.settings.applications.DrawOverlayDetails;
+import com.android.settings.emotion.Navbar;
+import com.android.settings.emotion.FlingSettings;
+import com.android.settings.emotion.SmartbarSettings;
 import com.android.settings.applications.InstalledAppDetails;
 import com.android.settings.applications.ManageApplications;
 import com.android.settings.applications.ManageAssist;
@@ -390,7 +393,10 @@ public class SettingsActivity extends Activity
             ContributorsCloudFragment.class.getName(),
             NotificationManagerSettings.class.getName(),
             LiveLockScreenSettings.class.getName(),
-            WeatherServiceSettings.class.getName()
+            WeatherServiceSettings.class.getName(),
+            Navbar.class.getName(),
+            FlingSettings.class.getName(),
+            SmartbarSettings.class.getName()
     };
 
 
