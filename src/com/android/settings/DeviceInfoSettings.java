3ed6a343318754833cb57e5e8a5514ced6c1c996
placiano <placiano80@gmail.com>
2016-05-09 22:05:56 +0200

    Revert "DeviceInfo: remove unused variable in getMemInfo()"
    This reverts commit 0677b9b77742513591e6b31e0df1e9d1826778e6.
    
    Signed-off-by: placiano <placiano80@gmail.com>

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -571,6 +571,7 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
 
     private String getMemInfo() {
         String result = null;
+        BufferedReader reader = null;
 
         try {
             /* /proc/meminfo entries follow this format:
